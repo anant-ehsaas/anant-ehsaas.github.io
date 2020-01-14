2548b40fcfe3531eb432747b8c43f07de15906a1
Saurabh Miglani <95smiglani@gmail.com>
2020-01-14 17:54:50 +0530

    added github pages link in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -35,6 +35,10 @@ const Footer = (props) => (
       <a href="https://html5up.net">HTML5 UP</a>. Built with:{' '}
       <a href="https://www.gatsbyjs.org/">Gatsby.js</a>
     </p>
+    <p className="copyright">
+     Hosted proudly on{' '}
+      <a href="https://pages.github.com/">GitHub Pages</a>
+    </p>
   </footer>
 )
 
