9d3de86995fc840b4ead1f96316b0b28e9371bb3
Saurabh Miglani <95smiglani@gmail.com>
2020-01-17 21:33:46 +0530

    minor update for footer formatting

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -34,11 +34,11 @@ const Footer = (props) => (
       &copy; Gatsby Starter - Dimension. Design:{' '}
       <a href="https://html5up.net">HTML5 UP</a>. Built with:{' '}
       <a href="https://www.gatsbyjs.org/">Gatsby.js</a>
-    </p>
-    <p className="copyright">
-     Hosted proudly on{' '}
+      <br />
+      Hosted proudly on{' '}
       <a href="https://pages.github.com/">GitHub Pages</a>
     </p>
+    
   </footer>
 )
 
