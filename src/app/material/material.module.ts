1c764ca143f894671246f66cad97af6414debc0f
Saurabh Miglani <saurabhmiglani@foliosolutions.in>
2021-01-10 22:19:25 +0530

    Add logo as mat-fab button

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
-const modules = [];
+const modules = [MatButtonModule];
 
 @NgModule({
   declarations: [],
