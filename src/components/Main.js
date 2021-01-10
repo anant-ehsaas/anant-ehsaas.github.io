be59c4161a4cff3a2fdf5bbd816974ad9bc101c9
codery2k <95smiglani@gmail.com>
2021-01-10 16:05:14 +0530

    Revert "removed dead link to projects"
    This reverts commit 195b1cb1bee8ccf74735ceebbe8f9aed294b82a2.

--- src/components/Main.js
+++ src/components/Main.js
@@ -77,9 +77,9 @@ class Main extends React.Component {
             the essence of service to humanity that redeems us all in the end.
             The message of AEF is boundless yet simple, 'Service is Joy'.
           </p>
-          {/* <p>
+          <p>
             By the way, check out our <a href="#work">projects</a>.
-          </p> */}
+          </p>
           {close}
         </article>
 
