2281bccf4c8fc9f5d8ab659249294eac2d5d9d1f
codery2k <95smiglani@gmail.com>
2021-01-21 21:22:46 +0530

    Create dashboard and vision component Define routes for them

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { VisionComponent } from './vision/vision.component';
 
-const appRoutes: Routes = [{}];
+const appRoutes: Routes = [
+  { path: 'vision', component: VisionComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+];
 
 @NgModule({
   declarations: [],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { AppRoutingModule } from './app-routing.module';
+import { VisionComponent } from './vision/vision.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, VisionComponent, DashboardComponent],
   imports: [BrowserModule, BrowserAnimationsModule, MaterialModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
