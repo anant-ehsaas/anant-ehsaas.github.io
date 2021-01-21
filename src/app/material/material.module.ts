95069ba3c896a6e3ca40b7c1318027b0ea29b3c2
codery2k <95smiglani@gmail.com>
2021-01-21 23:15:39 +0530

    Link and display 'vision' as a card

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
-const modules = [MatButtonModule];
+const modules = [
+  MatButtonModule,
+  MatCardModule,
+  MatDividerModule,
+  MatListModule,
+  MatIconModule,
+];
 
 @NgModule({
   declarations: [],
