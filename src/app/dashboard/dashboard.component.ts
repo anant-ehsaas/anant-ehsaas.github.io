af42049dad148aa095ab0e0bb6d0314f9a95b673
codery2k <95smiglani@gmail.com>
2021-01-24 14:51:07 +0530

    Update for closing modal with escape key Use mat-dialog instead of mat-card Convert contact-us to mat-dialog

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ContactUsComponent } from '../contact-us/contact-us.component';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
+  constructor(public dialog: MatDialog) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  openContactDialog(): void {
+    this.dialog.open(ContactUsComponent);
   }
-
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,20 +1,18 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDialogModule } from '@angular/material/dialog';
 
 const modules = [
   MatButtonModule,
-  MatCardModule,
-  MatDividerModule,
   MatListModule,
   MatIconModule,
   MatInputModule,
   MatFormFieldModule,
+  MatDialogModule,
 ];
 
 @NgModule({
